d974a2970362b38863c544ac403c2e091f91bbbe
eunkyeong <eunkyeong91@hanmail.net>
2023-01-21 12:07:46 +0900

    밥먹기전 commit

--- basic/basic.js
+++ basic/basic.js
@@ -155,3 +155,54 @@ passNonpass(70, 70);
 passNonpass(30, 100); 
 passNonpass(50, 50); 
 passNonpass(0, 120); 
+
+//연산자 변수 문제
+
+//1. 나이와 출신지역을 변수에 저장
+
+let age = 33;
+const homeTown ='경상남도 거창군';
+
+//2. 다음변수가 작동하지 않는 이유를 서술하시오
+
+let name ='park';
+let id=0;
+
+let showName = ()=>{
+    let name ='kim';
+    let id=1;
+    console.log(name);
+}
+showName(); //'kim' 이 풀력됨
+console.log(id); // 0이 출력됨
+
+/**
+ var-function scope, let, const-block scope를 가지기때문에 
+{}안에서 선언된 변수는 {}밖에서 할당,사용,접근이 불가하다. 
+전역변수로 선언된 변수는 {}안에서도 사용하능하다.
+*/
+
+//3. 이자율 계산하기
+/** 철수는 은향애 예금을 하러갔는데 예금금액에 따라 이자율이 변경됨
+    첫 예금액이 5만원 미만이면 이율이 연 15%
+    첫 예금액이 5만원 아상이면 이율이 연 20%
+    그래서 민준이는 
+    1. 변수에 예금금액을 넣으면 
+    2. 2년후에 총 예금액을 자동으로 출력해주는 기능을 스크립트로 짜려고하는데 
+    어떻게짜야함?
+ */
+
+
+
+   let interest = (deposit)=>{
+    let newDeposit =0;
+    if(deposit>=50000){
+    newDeposit = deposit *1.20 *1.20;}
+    else{newDeposit = deposit *1.15 *1.15;}
+    console.log(newDeposit);
+   }
+
+   interest(60000);
+   interest(10000);
+
+//
