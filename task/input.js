92974164551098e3cc7ffa9c1b3dc7078e193184
eunkyeong <eunkyeong91@hanmail.net>
2023-03-12 13:48:34 +0900

    add. 230312_Task_input,form

--- task/input.js
+++ task/input.js
@@ -1,5 +1,25 @@
-const EventForm = document.querySelector('#eventForm');
-const handup = (obj)=>{
-    
-    document.querySelector('.'+obj).classList.add('active');
+
+const infoActive= (obj)=>{
+     document.querySelector('.'+obj).classList.add('active');
 }
+const Name = document.querySelector('#name');
+const NameCheck = document.querySelector('#nameCheck');
+const Phone = document.querySelector('#phone');
+const PhoneCheck = document.querySelector('#phoneCheck');
+
+
+const FormCheck = ()=>{
+if(Name.value == ''){
+    NameCheck.innerHTML='아이디를 입력하세요'
+    Name.focus();
+    return false;
+}
+if(Phone.value == ''){
+    PhoneCheck.innerHTML='전화번호를 입력하세요.'
+    Phone.focus();
+    return false;
+}
+
+document.eventForm.submit();
+}
+// 유효성검사/ajax이용은 집에가서/
