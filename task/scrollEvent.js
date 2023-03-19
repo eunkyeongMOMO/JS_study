0fed0aaea6e4366af59b56f881b5282d3c35295a
eunkyeong <eunkyeong91@hanmail.net>
2023-03-19 21:30:00 +0900

    add. task_특정스크롤시 비디오 플레이v1

--- task/scrollEvent.js
+++ task/scrollEvent.js
@@ -2,3 +2,9 @@ const move_cont = (cont)=>{
     document.querySelector('.'+cont).scrollIntoView(true);
 }
 
+const VideoSample = document.querySelector('.video01');
+const VideoSampleHeight = VideoSample.offsetTop-300;
+console.log(VideoSampleHeight);
+window.addEventListener('scroll', ()=>{
+    VideoSample.play();
+})
