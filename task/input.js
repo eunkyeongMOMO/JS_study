89da3d0c4db1ff2eb4ba0376ea1334e1f503460f
eunkyeong <eunkyeong91@hanmail.net>
2023-04-18 23:41:09 +0900

    add.230418_button_click ui

--- task/input.js
+++ task/input.js
@@ -8,18 +8,25 @@ const Phone = document.querySelector('#phone');
 const PhoneCheck = document.querySelector('#phoneCheck');
 
 
-const FormCheck = ()=>{
-if(Name.value == ''){
-    NameCheck.innerHTML='아이디를 입력하세요'
-    Name.focus();
-    return false;
-}
-if(Phone.value == ''){
-    PhoneCheck.innerHTML='전화번호를 입력하세요.'
-    Phone.focus();
-    return false;
-}
+const formClick = ()=>{
+    let clickCount =0;
+    if(clickCount==0){
+        document.querySelector('.formWrap').classList.add('active');
+        clickCount++;
+    }else if(clickCount==1){
+        document.querySelector('.formWrap').classList.add('active');
+        if(Name.value == ''){
+            NameCheck.innerHTML='아이디를 입력하세요'
+            Name.focus();
+            return false;
+        }
+        if(Phone.value == ''){
+            PhoneCheck.innerHTML='전화번호를 입력하세요.'
+            Phone.focus();
+            return false;
+        }
 
-document.eventForm.submit();
+        // document.eventForm.submit();
+    }
 }
 // 유효성검사/ajax이용은 집에가서/
